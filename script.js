5fffc74181ba00c54c05f8d9bafe5d169bf04333
Yash Gupta <yashg3600@gmail.com>
2022-07-11 19:57:32 +0530

    final project

---
+++ script.js
@@ -0,0 +1,28 @@
+function myFunction()
+{
+    
+    document.getElementById("1").innerHTML = "Yash Gupta";
+    document.getElementById("2").innerHTML = "201500816";
+
+    document.getElementById("3").innerHTML =  "GLA University, Mathura";
+
+    document.getElementById("4").innerHTML = "11-07-2022  ";
+}
+
+
+
+
+
+
+
+     
+        // document.getElementById("1").innerHTML = "Yash Gupta";
+        // document.getElementById("2").innerHTML = "201500816";
+
+        // document.getElementById("3").innerHTML =  "GLA University, Mathura";
+
+        // document.getElementById("4").innerHTML = "11-07-2022  ";
+
+
+        
+       
